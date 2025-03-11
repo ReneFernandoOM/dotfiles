12de4d3e10fe64017dd2ef56fe6236c51b717de7
Rene Ortega <renefernandoortega@gmail.com>
2025-03-10 18:27:43 -0600

    improved diffview plugin

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -2,7 +2,8 @@ return {
   "sindrets/diffview.nvim",
   config = function()
     local diffview = require("diffview")
-    vim.keymap.set("n", "<leader>fh", "<cmd>DiffviewFileHistory %<cr>", { desc = "Open [F]ile [H]istory" })
+    vim.keymap.set("n", "<leader>fh", "<cmd>DiffviewFileHistory % --no-merges<cr>", { desc = "Open [F]ile [H]istory" })
+    vim.keymap.set("v", "<leader>fh", ":DiffviewFileHistory % --no-merges<cr>", { desc = "Open [F]ile [H]istory" })
     diffview.setup({
       view = {
         merge_tool = {
@@ -13,6 +14,9 @@ return {
         file_history = {
           disable_diagnostics = true,
           winbar_info = false,
+        },
+        git = {
+          log_args = { "--no-merges" }, -- Exclude merge commits
         },
       },
     })
