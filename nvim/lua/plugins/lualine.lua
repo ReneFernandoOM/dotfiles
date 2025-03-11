636860e721298f7016aa847eae326a849cabd4c0
Rene Ortega <renefernandoortega@gmail.com>
2025-03-10 18:28:51 -0600

    format

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,21 +1,21 @@
 return {
-	-- Set lualine as statusline
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = {
-			icons_enabled = true,
-			theme = "onedark",
-			component_separators = "|",
-			section_separators = "",
-		},
-		sections = {
-			lualine_a = { "mode", "branch" },
-			lualine_b = { "diff", "diagnostics" },
-		},
-		winbar = {
-			lualine_a = {},
-			lualine_b = { { "filename", path = 1, symbols = { modified = " ●" } } },
-		},
-	},
+  -- Set lualine as statusline
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = "onedark",
+      component_separators = "|",
+      section_separators = "",
+    },
+    sections = {
+      lualine_a = { "branch" },
+      lualine_b = { "diff", "diagnostics" },
+    },
+    winbar = {
+      lualine_a = {},
+      lualine_b = { { "filename", path = 1, symbols = { modified = " ●" } } },
+    },
+  },
 }
