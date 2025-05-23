ee4c0849dc12f0517f11a5412ead06d5bc8a7555
Rene Ortega <renefernandoortega@gmail.com>
2025-05-23 12:47:51 -0700

    Center screen after jumping to next hunk

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -12,8 +12,22 @@ return {
     },
     on_attach = function(bufnr)
       local gs = require("gitsigns")
-      vim.keymap.set("n", "<leader>gp", gs.prev_hunk, { buffer = bufnr, desc = "[G]o to [P]revious Hunk" })
-      vim.keymap.set("n", "<leader>gn", gs.next_hunk, { buffer = bufnr, desc = "[G]o to [N]ext Hunk" })
+
+      vim.keymap.set("n", "<leader>gp", function()
+        gs.prev_hunk()
+        -- presumably, gitsigns runs asyncronously so we need to have a small delay before centering the screen
+        vim.defer_fn(function()
+          vim.cmd("normal! zz")
+        end, 20)
+      end, { buffer = bufnr, desc = "[G]o to [P]revious Hunk" })
+
+      vim.keymap.set("n", "<leader>gn", function()
+        gs.next_hunk()
+        vim.defer_fn(function()
+          vim.cmd("normal! zz")
+        end, 20)
+      end, { buffer = bufnr, desc = "[G]o to [N]ext Hunk" })
+
       vim.keymap.set("n", "<leader>ph", gs.preview_hunk, { buffer = bufnr, desc = "[P]review [H]unk" })
       vim.keymap.set("n", "<leader>rh", gs.reset_hunk, { buffer = bufnr, desc = "[R]estore [H]unk" })
     end,
