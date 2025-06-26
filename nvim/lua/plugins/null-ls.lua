f1446bd20339cf63d4e7372d94ef60202aa928cf
Rene Ortega <renefernandoortega@gmail.com>
2025-06-26 09:38:57 -0700

    Swap null-ls for none-ls

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,26 +1,24 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = { "nvimtools/none-ls-extras.nvim" },
   event = { "LspAttach" },
   config = function()
     local null_ls = require("null-ls")
-    local code_actions = null_ls.builtins.code_actions
-    local diagnostics = null_ls.builtins.diagnostics
+
     local formatting = null_ls.builtins.formatting
 
     null_ls.setup({
+      debug = true,
       sources = {
         -- Typescript
-        formatting.prettier.with({
+        formatting.prettierd.with({
           prefer_local = "node_modules/.bin",
         }),
-        diagnostics.eslint.with({
-          prefer_local = "node_modules/.bin",
-        }),
-        code_actions.eslint_d.with({
-          prefer_local = "node_modules/.bin",
-        }),
+        require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.code_actions.eslint_d"),
         -- Python
-        formatting.ruff,
+        require("none-ls.formatting.ruff"),
+        require("none-ls.diagnostics.ruff"),
         -- Lua
         formatting.stylua,
       },
