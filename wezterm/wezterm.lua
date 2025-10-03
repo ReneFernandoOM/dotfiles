02dff3e60a31db66a16ca912daf173007f2ef31e
Rene Ortega <renefernandoortega@gmail.com>
2025-10-03 09:53:04 -0700

    Navigate text from terminal

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -34,6 +34,20 @@ config.keys = {
         mods = 'LEADER',
         action = wezterm.action.CloseCurrentPane { confirm = false },
     },
+    -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+    {
+        key = 'LeftArrow',
+        mods = 'OPT',
+        action = act.SendKey {
+            key = 'b',
+            mods = 'ALT',
+        },
+    },
+    {
+        key = 'RightArrow',
+        mods = 'OPT',
+        action = act.SendKey { key = 'f', mods = 'ALT' },
+    },
 }
 
 -- split navigation and resizing
